6ba6ace8a1be516c881ca40768c9ddb09d605c71
Shahazan Ali <kernel.writer@gmail.com>
2013-09-25 12:29:16 +0600

    - made some codebase enhancement

--- test/main.cpp
+++ test/main.cpp
@@ -6,31 +6,6 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 {
-    School school;
-    school.name("Kernel Coders Lab --- www.kernelcoderslab.com");
-    Address* address = new Address(&school);
-    address->country("Bangladesh");address->city("Rajshahi");address->zip("6402");
-    school.address(address);
-
-    Teacher* teacher = new Teacher(&school);
-    teacher->name("Mirza Alam");teacher->speciality(QStringList() << "C/C++" << "Algorithms");
-    address = new Address(teacher);
-    address->country("Bangladesh");address->city("Dhaka");address->zip("1000");
-    teacher->address(address);
-    school.appendTeacher(teacher);
-
-    Student* student = new Student(&school);
-    student->name("Shahazan Ali");student->courses(QStringList() << "C/C++" << "Algorithms");
-    address = new Address(student);
-    address->country("Bangladesh");address->city("Natore");address->zip("6200");
-    student->address(address);
-    school.appendStudent(student);
-
-    QByteArray json = school.exportToJson();
-    qDebug() << json;
-}
-
-{
     // Importing json into your custom instance
     QByteArray jsonInput = "{\"address\": {"
                 "\"city\": \"Rajshahi\","
@@ -63,5 +38,32 @@ int main(int argc, char *argv[])
     qDebug() <<"Teacer name: " << school.itemTeacherAt(0)->name();
     qDebug() <<"Student name: " << school.itemStudentAt(0)->name();
 }
+
+{
+    // Exporting json into your custom instance
+    School school;
+    school.name("Kernel Coders Lab --- www.kernelcoderslab.com");
+    Address* address = new Address(&school);
+    address->country("Bangladesh");address->city("Rajshahi");address->zip("6402");
+    school.address(address);
+
+    Teacher* teacher = new Teacher(&school);
+    teacher->name("Mirza Alam");teacher->speciality(QStringList() << "C/C++" << "Algorithms");
+    address = new Address(teacher);
+    address->country("Bangladesh");address->city("Dhaka");address->zip("1000");
+    teacher->address(address);
+    school.appendTeacher(teacher);
+
+    Student* student = new Student(&school);
+    student->name("Shahazan Ali");student->courses(QStringList() << "C/C++" << "Algorithms");
+    address = new Address(student);
+    address->country("Bangladesh");address->city("Natore");address->zip("6200");
+    student->address(address);
+    school.appendStudent(student);
+
+    QByteArray json = school.exportToJson();
+    qDebug() << json;
+}
+
     return a.exec();
 }
